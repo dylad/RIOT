404de14adcb194c325deda8775e0d18b4a5f2dc3
Dylan Laduranty <dylan.laduranty@mesotic.com>
2021-07-29 17:27:28 +0200

    pkg/nrf_modem: create initial pkg, include headers and load static lib

---
+++ tests/pkg_nrf_modem/main.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2021 Mesotic SAS
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief       Nordic nrf_modem example application for nRF9160-based MCUs
+ *
+ * @author      Dylan Laduranty <dylan.laduranty@mesotic.com>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "nrf_modem_os.h"
+
+
+int main(void)
+{
+    puts("nrf modem test application\n");
+   
+    return 0;
+}
