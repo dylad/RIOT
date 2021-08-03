53cda2f9d0070657a752ea25362284651f126232
dylad <dylan.laduranty@mesotic.com>
2021-08-03 21:16:01 +0200

    followup

--- pkg/nrf_modem/contrib/nrf_modem_hal.c
+++ pkg/nrf_modem/contrib/nrf_modem_hal.c
@@ -3,18 +3,28 @@
 #include "nrf_modem_platform.h"
 #include "nrf_modem_limits.h"
 
-#include "cpu.h"
 #include <errno.h>
+
+/* RIOT-OS specific headers */
+#include "nrf_modem_riot.h"
+#include "cpu.h"
 
 nrfx_err_t nrfx_ipc_init(uint8_t irq_priority, nrfx_ipc_handler_t handler, void * p_context)
 {
     (void) irq_priority;
     (void)handler;
     (void)p_context;
+    printf("[nrf_modem_hal]:%s\n", __FUNCTION__);
     return 0;
+}
+
+void nrfx_ipc_uninit(void)
+{
+    printf("[nrf_modem_hal]:%s\n", __FUNCTION__);
 }
 
 void nrfx_ipc_config_load(const nrfx_ipc_config_t * p_config)
 {
     (void)p_config;
+    printf("[nrf_modem_hal]:%s\n", __FUNCTION__);
 }
