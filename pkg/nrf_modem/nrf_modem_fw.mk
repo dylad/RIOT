b7512c329ef2ba6ae4382ebc019950536aff2671
Dylan Laduranty <dylan.laduranty@mesotic.com>
2021-10-21 18:22:04 +0200

    pkg/nrf_modem: add initial support

---
+++ pkg/nrf_modem/nrf_modem_fw.mk
@@ -0,0 +1,4 @@
+# Setup modem Firmware informations
+PKG_FW_NAME=mfw_nrf9160
+PKG_FW_URL=https://www.nordicsemi.com/-/media/Software-and-other-downloads/Dev-Kits/nRF9160-DK/nRF9160-modem-FW
+PKG_FW_VERSION=1.3.0
