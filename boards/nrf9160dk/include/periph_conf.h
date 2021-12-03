7c3b6304eba9c544db413eeaa689fecb8d49828f
dylad <dylan.laduranty@mesotic.com>
2021-12-03 22:43:57 +0100

    boards/nrf9160dk: add RTT configuration

--- boards/nrf9160dk/include/periph_conf.h
+++ boards/nrf9160dk/include/periph_conf.h
@@ -117,6 +117,25 @@ static const uart_conf_t uart_config[] = {
 #define UART_NUMOF          ARRAY_SIZE(uart_config) /**< UART confgiguration NUMOF */
 /** @} */
 
+
+/**
+ * @name    Real time counter configuration
+ * @{
+ */
+#ifndef RTT_DEV
+#define RTT_DEV             (1)                 /* NRF_RTC1 */
+#endif
+
+#define RTT_MAX_VALUE       (0x00ffffff)         /* 24bit */
+#define RTT_MAX_FREQUENCY   (32768U)             /* in Hz */
+#define RTT_MIN_FREQUENCY   (8U)                 /* in Hz */
+#define RTT_CLOCK_FREQUENCY (32768U)             /* in Hz, LFCLK*/
+
+#ifndef RTT_FREQUENCY
+#define RTT_FREQUENCY       (1024U)              /* in Hz */
+#endif
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif
